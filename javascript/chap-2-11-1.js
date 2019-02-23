3720e0115a47e498d713c97a0cc383346eed50ad
hou10kyoko <51ismaydaywhat@gmail.com>
2019-02-23 16:42:51 +0900

    first commit

---
+++ javascript/chap-2-11-1.js
@@ -0,0 +1,5 @@
+let text = prompt('年齢は？');
+let age = parseInt (text);
+if ( age < 6 ){
+  console.log('幼児');
+}
---
+++ javascript/chap2-5-3.js
@@ -0,0 +1,7 @@
+let text = prompt('年齢は？');
+let age = parseInt(text);
+if(age<20){
+  console.log('未成年');
+}else{
+  console.log('成年');
+}
